b3b2ca7a6a51df228935777990f097f86789abd8
Maurillugdar <maurillugdar@gmail.com>
2025-05-24 20:47:59 -0300

    Initial commit - Spring Boot app

---
+++ src/main/java/dev/maullu/runnerz/RunnerzApplication.java
@@ -0,0 +1,17 @@
+package dev.maullu.runnerz;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerzApplication {
+	public static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerzApplication.class, args);
+		log.info("Application has logged succesfully!");
+	}
+
+}
