a2ab4390c8b97bce5b7ee905a047bda40b146fb9
Maurillugdar <maurillugdar@gmail.com>
2025-05-26 15:50:18 -0300

    FEAT: Add validation to the Run class

--- src/main/java/dev/maullu/runnerz/run/Run.java
+++ src/main/java/dev/maullu/runnerz/run/Run.java
@@ -1,13 +1,23 @@
 package dev.maullu.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
-public record Run(
+public record Run( // In a record the attributes could be used as func. bcs that's the getter
         Integer id,
+        @NotEmpty //Using validation
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive
         Integer miles,
         Location location
-) {
+) { // This compact constructor let us validate or modify values of the instances
+    public Run{
+        if (startedOn.isAfter(completedOn)){
+            throw new IllegalArgumentException("completedOn must be after startedOn");
+        }
+    }
 }
