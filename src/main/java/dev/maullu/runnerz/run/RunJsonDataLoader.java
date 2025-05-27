fb901d28b63fe358792230e89c1e8068eb8ecb78
Maurillugdar <maurillugdar@gmail.com>
2025-05-27 01:35:51 -0300

    REFACTOR: After rename of runRepo change variables

--- src/main/java/dev/maullu/runnerz/run/RunJsonDataLoader.java
+++ src/main/java/dev/maullu/runnerz/run/RunJsonDataLoader.java
@@ -1,31 +1,33 @@
 package dev.maullu.runnerz.run;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
 
-@Component // We tell Spring to be in charge of this class
+ // We tell Spring to be in charge of this class
+@Component
 public class RunJsonDataLoader implements CommandLineRunner { // CommandLineRunner is used to execute code after the application has started
 
-    private final RunRepository runRepository;
+    private final JdbcClientRunRepository jdbcClientRunRepository;
     private final ObjectMapper objectMapper;
 
-    public RunJsonDataLoader(RunRepository runRepository, ObjectMapper objectMapper) {
-        this.runRepository = runRepository;
+    public RunJsonDataLoader(JdbcClientRunRepository jdbcClientRunRepository, ObjectMapper objectMapper) {
+        this.jdbcClientRunRepository = jdbcClientRunRepository;
         this.objectMapper = objectMapper;
     }
 
     @Override
     public void run(String... args) throws Exception { // This is the method that is called
-        if (runRepository.count() == 0) {
+        if (jdbcClientRunRepository.count() == 0) {
             try (InputStream inputStream = getClass().getResourceAsStream("/data/runs.json")) { // Load the JSON file from resources
                 if (inputStream == null) {
                     throw new IllegalArgumentException("Data file not found");
                 }
                 Runs allRuns = objectMapper.readValue(inputStream, Runs.class);
-                runRepository.saveAll(allRuns.runs());
+                jdbcClientRunRepository.saveAll(allRuns.runs());
             } catch (Exception e) {
                 throw new RuntimeException("Failed to load runs from JSON", e);
             }
