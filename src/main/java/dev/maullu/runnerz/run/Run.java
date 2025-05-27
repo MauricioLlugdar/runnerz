789cffdfa795796883cdf47b3f5fd63ca216724b
Maurillugdar <maurillugdar@gmail.com>
2025-05-27 01:34:25 -0300

    FEAT: Add an extra attribute and an annotation

--- src/main/java/dev/maullu/runnerz/run/Run.java
+++ src/main/java/dev/maullu/runnerz/run/Run.java
@@ -2,10 +2,13 @@ package dev.maullu.runnerz.run;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 
 import java.time.LocalDateTime;
 
 public record Run( // In a record the attributes could be used as func. bcs that's the getter
+        @Id
         Integer id,
         @NotEmpty //Using validation
         String title,
@@ -13,7 +16,9 @@ public record Run( // In a record the attributes could be used as func. bcs that
         LocalDateTime completedOn,
         @Positive
         Integer miles,
-        Location location
+        Location location,
+        @Version
+        Integer version // Check if it is a new row or an existing
 ) { // This compact constructor let us validate or modify values of the instances
     public Run{
         if (startedOn.isAfter(completedOn)){
