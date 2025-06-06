79ec1f01f22aa4b927450187c882bfa17801f82f
Maurillugdar <maurillugdar@gmail.com>
2025-06-06 16:51:56 -0300

    FEAT and REF: Use completly the RunRepository instead of creating the methods to communicate w db

--- src/main/java/dev/maullu/runnerz/run/RunJsonDataLoader.java
+++ src/main/java/dev/maullu/runnerz/run/RunJsonDataLoader.java
@@ -11,23 +11,23 @@ import java.io.InputStream;
 @Component
 public class RunJsonDataLoader implements CommandLineRunner { // CommandLineRunner is used to execute code after the application has started
 
-    private final JdbcClientRunRepository jdbcClientRunRepository;
+    private final RunRepository runRepository;
     private final ObjectMapper objectMapper;
 
-    public RunJsonDataLoader(JdbcClientRunRepository jdbcClientRunRepository, ObjectMapper objectMapper) {
-        this.jdbcClientRunRepository = jdbcClientRunRepository;
+    public RunJsonDataLoader(RunRepository runRepository, ObjectMapper objectMapper) {
+        this.runRepository = runRepository;
         this.objectMapper = objectMapper;
     }
 
     @Override
     public void run(String... args) throws Exception { // This is the method that is called
-        if (jdbcClientRunRepository.count() == 0) {
+        if (runRepository.count() == 0) {
             try (InputStream inputStream = getClass().getResourceAsStream("/data/runs.json")) { // Load the JSON file from resources
                 if (inputStream == null) {
                     throw new IllegalArgumentException("Data file not found");
                 }
                 Runs allRuns = objectMapper.readValue(inputStream, Runs.class);
-                jdbcClientRunRepository.saveAll(allRuns.runs());
+                runRepository.saveAll(allRuns.runs());
             } catch (Exception e) {
                 throw new RuntimeException("Failed to load runs from JSON", e);
             }
