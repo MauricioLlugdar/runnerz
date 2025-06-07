e0a98a0c1175e0a7aea0598d3f2534725662e79f
Maurillugdar <maurillugdar@gmail.com>
2025-06-07 01:02:42 -0300

    TEST: Some tests added for testing some functionalities

--- src/test/java/dev/maullu/runnerz/run/RunRepositoryTest.java
+++ src/test/java/dev/maullu/runnerz/run/RunRepositoryTest.java
@@ -1,5 +1,6 @@
 package dev.maullu.runnerz.run;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
@@ -9,6 +10,8 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,4 +31,62 @@ class RunRepositoryTest {
         assertTrue(postgreSQL.isCreated());
         assertTrue(postgreSQL.isRunning());
     }
+
+
+    @BeforeEach
+    void setUp() {
+        runRepository.saveAll(List.of(new Run(
+                1,
+                "Test Run",
+                java.time.LocalDateTime.of(2023, 10, 1, 9, 0),
+                java.time.LocalDateTime.of(2023, 10, 1, 10, 0),
+                5,
+                Location.OUTDOOR,
+                null
+            ),
+            new Run(
+                2,
+                "Another Test Run",
+                java.time.LocalDateTime.of(2023, 10, 2, 9, 0),
+                java.time.LocalDateTime.of(2023, 10, 2, 10, 0),
+                10,
+                Location.INDOOR,
+                null
+            ),
+            new Run(
+                3,
+                "Third Test Run",
+                java.time.LocalDateTime.of(2023, 10, 3, 9, 0),
+                java.time.LocalDateTime.of(2023, 10, 3, 10, 0),
+                15,
+                Location.OUTDOOR,
+                null
+            ))
+        );
+    }
+
+    @Test
+    void findAll() {
+        List<Run> runs = runRepository.findAll();
+        assertEquals(3, runs.size());
+    }
+
+    @Test
+    void findById() {
+        Run run = runRepository.findById(1).orElseThrow();
+        assertEquals("Test Run", run.title());
+    }
+
+    @Test
+    void findAllByLocation() {
+        List<Run> outdoorRuns = runRepository.findAllByLocation("OUTDOOR");
+        assertEquals(2, outdoorRuns.size());
+        assertTrue(outdoorRuns.stream().allMatch(run -> run.location() == Location.OUTDOOR));
+
+        List<Run> indoorRuns = runRepository.findAllByLocation("INDOOR");
+        assertEquals(1, indoorRuns.size());
+        assertTrue(indoorRuns.stream().allMatch(run -> run.location() == Location.INDOOR));
+    }
+
+
 }
