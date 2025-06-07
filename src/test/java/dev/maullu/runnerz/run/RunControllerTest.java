9425b6eefa1233dcd249998b9b9bea9d3c4f32fb
Maurillugdar <maurillugdar@gmail.com>
2025-06-07 01:32:46 -0300

    TEST: Add Integration Test

---
+++ src/test/java/dev/maullu/runnerz/run/RunControllerTest.java
@@ -0,0 +1,38 @@
+package dev.maullu.runnerz.run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//INTEGRATION TEST
+
+@Testcontainers // Scan for container annotation
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RunControllerTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:17.0");
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+
+    @Test
+    void shouldFindAllRuns(){
+        // "/api/runs"
+        Run[] runs = restTemplate.getForObject("/api/runs", Run[].class);
+        assertEquals(10, runs.length, "There should be 10 runs in the database");
+    }
+
+}
