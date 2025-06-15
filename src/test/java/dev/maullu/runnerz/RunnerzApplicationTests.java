eaa32b33329585ad0823a237d7643e9e6184e7e2
Maurillugdar <maurillugdar@gmail.com>
2025-06-15 18:14:39 -0300

    TEST: Add context of the testContainers in the context test

--- src/test/java/dev/maullu/runnerz/RunnerzApplicationTests.java
+++ src/test/java/dev/maullu/runnerz/RunnerzApplicationTests.java
@@ -2,9 +2,17 @@ package dev.maullu.runnerz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
 class RunnerzApplicationTests {
+
+	// Add the @Testcontainers annotation to enable Testcontainers support
+	@Container //Docker container in this case
+	@ServiceConnection //It configures automatically the testDb
+	static PostgreSQLContainer<?> postgreSQL = new PostgreSQLContainer<>("postgres:17"); // I want to use the same than in production
 
 	@Test
 	void contextLoads() {
